0ae6a88f64d4cebb0f2dc159e6942e0d7ab51466
Gleb <maxwellgleb@gmail.com>
2021-09-17 01:56:22 +0300

    Create index view and pattern for it in urls.py. Create templates folder and template for IndexView. Insert several rows in database.

--- inplaces/placegallery/views.py
+++ inplaces/placegallery/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
 
-class IndexView(generic.ListView):
-    pass
+from .models import City
 
-class CityView(generic.ListView):
+
+class IndexView(generic.ListView):
+    template_name = 'placegallery/index.html'
+    context_object_name = 'alphabetical_order_city_list'
+
+    def get_queryset(self):
+        return City.objects.all().order_by('city_name')[:5]
+
+
+class CityView(generic.DetailView):
     pass
