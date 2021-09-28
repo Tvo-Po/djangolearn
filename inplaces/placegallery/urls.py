ce7b38155a88a21b9490ec94bbbd17e73c55b60e
Gleb <maxwellgleb@gmail.com>
2021-09-28 21:34:14 +0300

    DB fix

--- inplaces/placegallery/urls.py
+++ inplaces/placegallery/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'placegallery'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<str:slug>/', views.CityView.as_view(), name='city'),
-    path('<str:city_name>/<str:slug>', views.InterestingPlaceView.as_view(), name='place'),
+    path('<slug:slug>/', views.CityView.as_view(), name='city'),
+    path('<str:city_name>/<slug:slug>', views.InterestingPlaceView.as_view(), name='place'),
 ]
