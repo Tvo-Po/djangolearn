913443d72b3d2bc7744d8c080404f1a0a170d79b
Gleb <maxwellgleb@gmail.com>
2021-09-29 20:34:33 +0300

    Add simple pagination

--- inplaces/placegallery/views.py
+++ inplaces/placegallery/views.py
@@ -7,9 +7,10 @@ from .models import City, InterestingPlace
 class IndexView(generic.ListView):
     template_name = 'placegallery/index.html'
     context_object_name = 'alphabetical_order_city_list'
+    paginate_by = 4
 
     def get_queryset(self):
-        return City.objects.order_by('city_name_ru')[:5]
+        return City.objects.order_by('city_name_ru')[:]
 
 
 class CityView(generic.DetailView):
